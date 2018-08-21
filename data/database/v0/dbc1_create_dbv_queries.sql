9f9f10f0d0ba1c79e802c5b152e564716824d4a6
danielhemmerich <danielhemmerich1985@gmail.com>
2018-08-21 18:31:08 +0200

    removed null from primary key

--- data/database/v0/dbc1_create_dbv_queries.sql
+++ data/database/v0/dbc1_create_dbv_queries.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `dbv_queries` (
   `version` bigint(20) unsigned NOT NULL,
   `name` varchar(128) NOT NULL DEFAULT '',
   `datetime` datetime NOT NULL,
-  `hash` varchar(32) DEFAULT NULL,
+  `hash` varchar(32),
   `query` longblob NOT NULL,
   PRIMARY KEY (`version`,`name`,`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
